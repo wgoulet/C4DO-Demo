e32bda3ad9826b25a77986383b83aad5666efb4d
walter.goulet <walter.goulet@venafi.com>
2019-08-19 09:42:15 -0500

    Changed to use environment variables

--- run.sh
+++ run.sh
@@ -10,14 +10,14 @@ generate_post_data()
 EOF
 }
 
-echo $1
-echo $2
+echo $CONTAINER_FQDN
+echo $VAULT_TOKEN
 
-CERTSERIAL=`curl -k -H "X-Vault-Token: $2" -X POST https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/issue/cloud-backend -d "$(generate_post_data $1)" | jq '.data.serial_number' -j | sed -e s/:/-/g`
+CERTSERIAL=`curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X POST https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/issue/cloud-backend -d "$(generate_post_data $CONTAINER_FQDN)" | jq '.data.serial_number' -j | sed -e s/:/-/g`
 echo $CERTSERIAL
 
-curl -k -H "X-Vault-Token: $2" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j 
-curl -k -H "X-Vault-Token: $2" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j >> /etc/ssl/nginx.crt
-curl -k -H "X-Vault-Token: $2" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.private_key' -j >> /etc/ssl/nginx.key
+curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j 
+curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j >> /etc/ssl/nginx.crt
+curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.private_key' -j >> /etc/ssl/nginx.key
 nginx -g "daemon off;"
 
