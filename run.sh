8b3393748e8ddef51860657915e9335bb7600ead
walter.goulet <walter.goulet@venafi.com>
2019-08-19 21:09:17 -0500

    Changed to fix wrong variables in startup script

--- run.sh
+++ run.sh
@@ -9,15 +9,16 @@ generate_post_data()
 }
 EOF
 }
-
+CONTAINER_FQDN=$1
+VAULT_TOKEN=$2
 echo $CONTAINER_FQDN
 echo $VAULT_TOKEN
+
 
 CERTSERIAL=`curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X POST https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/issue/cloud-backend -d "$(generate_post_data $CONTAINER_FQDN)" | jq '.data.serial_number' -j | sed -e s/:/-/g`
 echo $CERTSERIAL
 
-curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j 
+curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j
 curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j >> /etc/ssl/nginx.crt
 curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.private_key' -j >> /etc/ssl/nginx.key
 nginx -g "daemon off;"
-
