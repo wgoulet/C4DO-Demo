39b5d79d890e88a8b49805833262cc865f84ab25
walter.goulet <walter.goulet@venafi.com>
2019-08-20 16:25:41 -0500

    Parameterized URLs for Vault

--- run.sh
+++ run.sh
@@ -11,14 +11,16 @@ EOF
 }
 CONTAINER_FQDN=$1
 VAULT_TOKEN=$2
+VAULT_ROLE_URL=$3
+VAULT_PIKCUP_URL=$4
 echo $CONTAINER_FQDN
 echo $VAULT_TOKEN
 
 
-CERTSERIAL=`curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X POST https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/issue/cloud-backend -d "$(generate_post_data $CONTAINER_FQDN)" | jq '.data.serial_number' -j | sed -e s/:/-/g`
+CERTSERIAL=`curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X POST $3 -d "$(generate_post_data $CONTAINER_FQDN)" | jq '.data.serial_number' -j | sed -e s/:/-/g`
 echo $CERTSERIAL
 
-curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j
-curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.certificate_chain' -j >> /etc/ssl/nginx.crt
-curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET https://vcloudvault.centralus.cloudapp.azure.com:8200/v1/venafi-pki/cert/$CERTSERIAL | jq '.data.private_key' -j >> /etc/ssl/nginx.key
+curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET $4$CERTSERIAL | jq '.data.certificate_chain' -j
+curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET $4$CERTSERIAL | jq '.data.certificate_chain' -j >> /etc/ssl/nginx.crt
+curl -k -H "X-Vault-Token: $VAULT_TOKEN" -X GET $4$CERTSERIAL | jq '.data.private_key' -j >> /etc/ssl/nginx.key
 nginx -g "daemon off;"
