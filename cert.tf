d54ce4b89710a82a579138eefb31687ed4b6cdc0
Walter Goulet <wgoulet@gmail.com>
2019-10-03 01:19:26 +0000

    Add example terraform plan for creating a cert and saving it to p12 format

---
+++ cert.tf
@@ -0,0 +1,63 @@
+variable "common_name" {
+	type = string
+}
+
+variable "dns_sans" {
+	type = list(string)
+}
+
+variable "passphrase" {
+	type = string
+}
+
+variable "apikey" {
+	type = string
+}
+
+variable "zone" {
+	type = string
+}
+
+provider "venafi" {
+	api_key = "${var.apikey}"
+	zone = "${var.zone}"
+}
+
+resource "venafi_certificate" "webserver" {
+    common_name = "${var.common_name}"
+    algorithm = "RSA"
+    rsa_bits = "2048"
+    san_dns = "${var.dns_sans}"
+    key_password = "${var.passphrase}"
+}
+
+
+resource "local_file" "privatekey" {
+    content = "${venafi_certificate.webserver.private_key_pem}"
+    filename = "${path.module}/privkey.pem"
+}
+
+resource "local_file" "chain" {
+    content = "${venafi_certificate.webserver.chain}${venafi_certificate.webserver.certificate}"
+    filename = "${path.module}/certs.pem"
+}
+
+output "cert_certificate" {
+    value = "${venafi_certificate.webserver.certificate}"
+}
+
+output "cert_chain" {
+    value = "${venafi_certificate.webserver.chain}"
+}
+
+output "cert_private_key" {
+    value = "${venafi_certificate.webserver.private_key_pem}"
+    sensitive   = false
+}
+
+resource "null_resource" "create-p12" {
+    provisioner "local-exec" {
+        command = "openssl pkcs12 -export -in '${local_file.chain.filename}' -inkey '${local_file.privatekey.filename}' -passin pass:\"${var.passphrase}\" -password pass:\"${var.passphrase}\" -out ./tcert.p12"
+    }
+}
+
